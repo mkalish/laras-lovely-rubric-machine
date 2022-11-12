3cac842abf490a5931eec30b3f5a2f521aaeda70
Michael Kalish <michael@spanishdict.com>
2022-11-12 14:18:58 -0500

    More tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,24 @@ import {
   Grid,
   GridItem,
   Input,
+  Heading,
+  Divider,
+  FormControl,
+  FormLabel,
+  Stack,
+  Box,
 } from "@chakra-ui/react";
 
 const calculateScore = (
   studentsScore: number,
   possiblePoints: number,
   possiblePointsInGradebook: number
-): number => {
-  return Math.round(
-    (studentsScore / possiblePoints) * possiblePointsInGradebook
+): string => {
+  if (studentsScore === 0) {
+    return "0";
+  }
+  return ((studentsScore / possiblePoints) * possiblePointsInGradebook).toFixed(
+    1
   );
 };
 
@@ -26,50 +35,62 @@ function App() {
     <ChakraProvider>
       <Container>
         <div className="App">
-          <h1>Lara's Lovely Rubric Machine</h1>
-          <Grid templateRows="repeat(4, 1fr)" gap={4}>
-            <GridItem>
-              <p>Assignment points value</p>
-              <Input
-                type="number"
-                onChange={(evt) =>
-                  setPossiblePoints(parseInt(evt.target.value, 10))
-                }
-                placeholder="How many points the assignment is worth"
-              />
-            </GridItem>
-            <GridItem>
-              <p>Gradebook points value</p>
-              <Input
-                type="number"
-                onChange={(evt) =>
-                  setPossibleGradebookPoints(parseInt(evt.target.value, 10))
-                }
-                placeholder="How many points the assignment is worth in the gradebook"
-              />
-            </GridItem>
-            <GridItem>
-              <p>Score</p>
-              <Input
-                type="number"
-                onChange={(evt) =>
-                  setStudentScore(parseInt(evt.target.value, 10))
-                }
-                placeholder="Student Score"
-              />
-            </GridItem>
-            <GridItem>
-              <p>Assignment points value</p>
-              <p>
+          <Heading>Lara's Lovely Rubric Machine</Heading>
+          <Divider />
+          <Stack direction={"column"} marginTop={5}>
+            <Box>
+              <FormControl>
+                <FormLabel>Assignment points value</FormLabel>
+                <Input
+                  type="number"
+                  onChange={(evt) =>
+                    setPossiblePoints(parseInt(evt.target.value, 10))
+                  }
+                  defaultValue={possiblePoints}
+                  placeholder="How many points the assignment is worth"
+                />
+              </FormControl>
+            </Box>
+            <Box>
+              <FormControl>
+                <FormLabel>Gradebook points value</FormLabel>
+                <Input
+                  type="number"
+                  onChange={(evt) =>
+                    setPossibleGradebookPoints(parseInt(evt.target.value, 10))
+                  }
+                  defaultValue={possibleGradebookPoints}
+                  placeholder="How many points the assignment is worth in the gradebook"
+                />
+              </FormControl>
+            </Box>
+            <Box>
+              <FormControl>
+                <FormLabel>Score</FormLabel>
+                <Input
+                  type="number"
+                  onChange={(evt) =>
+                    setStudentScore(parseInt(evt.target.value, 10))
+                  }
+                  placeholder="Student Score"
+                />
+              </FormControl>
+            </Box>
+            <Divider marginTop={10} marginBottom={10} />
+            <Box>
+              <Heading marginTop={5} as={"h2"}>
+                Assignment points value
+              </Heading>
+              <Heading as={"h3"}>
                 {studentScore &&
                   calculateScore(
                     studentScore,
                     possiblePoints,
                     possibleGradebookPoints
                   )}
-              </p>
-            </GridItem>
-          </Grid>
+              </Heading>
+            </Box>
+          </Stack>
         </div>
       </Container>
     </ChakraProvider>
